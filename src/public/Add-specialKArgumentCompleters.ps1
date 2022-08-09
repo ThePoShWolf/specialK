24890de7726cbf3862f88f15a75b43f47bd333ff
Anthony Howell <anthony@howell-it.com>
2022-08-09 06:17:14 -0700

    adding arg completer

---
+++ src/public/Add-specialKArgumentCompleters.ps1
@@ -0,0 +1,21 @@
+# https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-pwsh/
+# but we are going to switch the completer to 'k' from 'kubectl'
+Function Add-specialKArgumentCompleters {
+    [cmdletbinding()]
+    param (
+        [switch]$ToProfile
+    )
+    $toReplace = "-CommandName 'kubectl'"
+    $argCompleter = (kubectl completion powershell) -replace $toReplace, "-CommandName 'k'"
+    if ($argCompleter -like 'error*') {
+        throw $argCompleter
+    }
+
+    if ($null -ne $argCompleter) {
+        $argCompleter | Out-String | Invoke-Expression
+    }
+
+    if ($ToProfile.IsPresent) {
+        $argCompleter | Out-File $PROFILE -Append
+    }
+}
