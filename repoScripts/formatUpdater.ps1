bbaa2b6355ef6f2f4efbad094b34632f2c155539
Anthony Howell <anthony@howell-it.com>
2022-08-09 04:47:45 -0700

    adding format

---
+++ repoScripts/formatUpdater.ps1
@@ -0,0 +1,63 @@
+$baseTemplate = @"
+<Configuration>
+	<ViewDefinitions>
+		<view></view>
+	</ViewDefinitions>
+</Configuration>
+"@
+
+$view = @"
+        <View>
+            <Name>{NAME}</Name>
+            <ViewSelectedBy>
+                <TypeName>{NAME}</TypeName>
+            </ViewSelectedBy>
+            <TableControl>
+                <TableHeaders>
+{HEADER}
+                </TableHeaders>
+                <TableRowEntries>
+                    <TableRowEntry>
+                        <TableColumnItems>
+{COLUMNITEM}
+                        </TableColumnItems>
+                    </TableRowEntry>
+                </TableRowEntries>
+            </TableControl>
+        </View>
+"@
+
+$tcHeader = @"
+<TableColumnHeader><Label>{HEADER}</Label></TableColumnHeader>
+"@
+
+$tcItem = @"
+<TableColumnItem><PropertyName>{COLUMNITEM}</PropertyName></TableColumnItem>
+"@
+
+$commands = @{
+    get    = @(
+        'pod', 'deployment', 'node'
+    )
+    top    = @(
+        'pod', 'node'
+    )
+    config = @(
+        'get-contexts'
+    )
+}
+$addViews = foreach ($command in $commands.Keys) {
+    foreach ($sub in $commands[$command]) {
+        $newView = $view -replace '\{NAME\}', "$command-$sub"
+        $props = (k $command $sub)[0].psobject.properties.name
+        $headers = foreach ($header in $props) {
+            $tcHeader -replace '\{HEADER\}', $header
+        }
+        $tcItems = foreach ($tci in $props) {
+            $tcItem -replace '\{COLUMNITEM\}', $tci
+        }
+        $newView -replace '\{HEADER\}', ($headers -join "`n") -replace '\{COLUMNITEM\}', ($tcItems -join "`n")
+    }
+}
+
+$baseTemplate -replace [regex]::Escape('<view></view>'), ($addViews -join "`n") | Out-File $PSScriptRoot\..\build\specialK\k.format.ps1xml -Force
