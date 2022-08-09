70990e20d48c38a24f54e7e6f982fc42c096b22f
Anthony Howell <anthony@howell-it.com>
2022-08-09 05:47:45 -0700

    improving logic for supported commands

--- src/public/k.ps1
+++ src/public/k.ps1
@@ -1,10 +1,12 @@
 function k {
     if ($objectCommands.Keys -contains $args[0] -and $args[1] -ne '--help') {
-        $typeName = "$($args[0])-$($args[1])"
-        # Convert the output to CSV by replacing multiple spaces with a comma
-        # then add a type name to use the formatter
-        (& kubectl $args) -replace '  +', ',' | ForEach-Object { $_.trim(',') } | ConvertFrom-Csv | ForEach-Object { $_.PSObject.TypeNames.Insert(0, $typeName); $_ }
-    } else {
-        & kubectl $args
+        if ($objectCommands[$args[0]] -contains $args[1]) {
+            $typeName = "$($args[0])-$($args[1])"
+            # Convert the output to CSV by replacing multiple spaces with a comma
+            # then add a type name to use the formatter
+            (& kubectl $args) -replace '  +', ',' | ForEach-Object { $_.trim(',') } | ConvertFrom-Csv | ForEach-Object { $_.PSObject.TypeNames.Insert(0, $typeName); $_ }
+            return
+        }
     }
+    & kubectl $args
 }
