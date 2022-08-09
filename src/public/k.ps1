c9effc2b1a780858539cdebbe24efbde87b70e2f
Anthony Howell <anthony@howell-it.com>
2022-08-09 04:59:12 -0700

    expanding aliases

--- src/public/k.ps1
+++ src/public/k.ps1
@@ -2,7 +2,7 @@ function k {
     $objectCommands = @('top', 'get')
     if ($objectCommands -contains $args[0] -and $args[1] -ne '--help') {
         $typeName = "$($args[0])-$($args[1])"
-        (& kubectl $args) -replace '  +', ',' | % { $_.trim(',') } | ConvertFrom-Csv | % { $_.PSObject.TypeNames.Insert(0, $typeName); $_ }
+        (& kubectl $args) -replace '  +', ',' | ForEach-Object { $_.trim(',') } | ConvertFrom-Csv | ForEach-Object { $_.PSObject.TypeNames.Insert(0, $typeName); $_ }
     } else {
         & kubectl $args
     }
