518c9f6dabcc9978fd6581ce992324465a31fe4c
Anthony Howell <anthony@howell-it.com>
2022-08-09 05:00:28 -0700

    adding some comments

--- src/public/k.ps1
+++ src/public/k.ps1
@@ -2,6 +2,8 @@ function k {
     $objectCommands = @('top', 'get')
     if ($objectCommands -contains $args[0] -and $args[1] -ne '--help') {
         $typeName = "$($args[0])-$($args[1])"
+        # Convert the output to CSV by replacing multiple spaces with a comma
+        # then add a type name to use the formatter
         (& kubectl $args) -replace '  +', ',' | ForEach-Object { $_.trim(',') } | ConvertFrom-Csv | ForEach-Object { $_.PSObject.TypeNames.Insert(0, $typeName); $_ }
     } else {
         & kubectl $args
