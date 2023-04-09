b933aaaf8ee07542edd9a834e24255c11b39e720
Anthony Howell <anthony@howell-it.com>
2023-04-09 11:53:25 -0700

    checking output rather than parameters

--- src/public/k.ps1
+++ src/public/k.ps1
@@ -1,13 +1,13 @@
 function k {
-    if ($objectCommands.Keys -contains $args[0] -and $args[1] -ne '--help' -and $args.Count -eq 2) {
-        if ($objectCommands[$args[0]] -contains $args[1]) {
-            # select the format type name
-            $typeName = "$($args[0])-$($args[1])"
-
-            # get the output
-            $out = (& kubectl $args)
-
-            # locate all positions to place semicolons
+    $skipArgs = @(
+        'exec', 'cp', 'scale', 'rollout', 'delete', 'logs'
+    )
+    if ($skipArgs -contains $args[0]) {
+        & kubectl $args
+    } else {
+        $out = (& kubectl $args)
+        if ($out[0] -match '^(NAME|CURRENT)') {
+            # locate all positions to place commas
             # we are using the headers since some values may be null in the data
             if ($null -ne $out) {
                 $m = $out[0] | Select-String -Pattern '  \S' -AllMatches
@@ -21,10 +21,15 @@ function k {
                 $line
             }
 
-            # convert from csv (since we added semicolons)
-            $out -replace ' +;', ';' | ForEach-Object { $_.Trim() } | ConvertFrom-Csv -Delimiter ';' | ForEach-Object { $_.PSObject.TypeNames.Insert(0, $typeName); $_ }
-            return
+            if ($objectCommands[$args[0]] -contains $args[1]) {
+                # select the format type name
+                $typeName = "$($args[0])-$($args[1])"
+                $out -replace ' +;', ';' | ForEach-Object { $_.Trim() } | ConvertFrom-Csv -Delimiter ';' | ForEach-Object { $_.PSObject.TypeNames.Insert(0, $typeName); $_ }
+            } else {
+                $out -replace ' +;', ';' | ForEach-Object { $_.Trim() } | ConvertFrom-Csv -Delimiter ';'
+            }
+        } else {
+            $out
         }
     }
-    & kubectl $args
 }
